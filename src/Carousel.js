d2f942640fb7c04488346edf1f4fa546f261bbfa
TingYu Wong <sandy20615@gmail.com>
2022-04-22 08:01:43 +0800

    class component basics

---
+++ src/Carousel.js
@@ -0,0 +1,41 @@
+import { Component } from "react"
+
+class Carousel extends Component {
+  constructor() {
+    super()
+    this.state = { active: 0 }
+  }
+
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  }
+
+  handleIndexClick = (e) => {
+    this.setState({ active: e.target.dataset.index })
+  }
+
+  render() {
+    const { active } = this.state
+    const { images } = this.props
+
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            <img 
+              alt="animal thumbnail"
+              key={photo}
+              src={photo}
+              data-index={index}
+              className={index == active ? 'active' : ''}
+              onClick={this.handleIndexClick}
+            />
+          ))}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Carousel
