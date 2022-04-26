dc996bee03694a27e700967eab60330c6b4ac025
翁婷瑜 <t13736@ms.sltung.com.tw>
2022-04-26 17:28:35 +0800

    Refactor Modal.js to tsx

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,19 +1,22 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, FunctionComponent, MutableRefObject } from "react"
 import { createPortal } from "react-dom"
 
-const Modal = ({children}) => {
-  const elRef = useRef(null)
+const Modal: FunctionComponent = ({children}) => {
+
+  const elRef: MutableRefObject<HTMLDivElement | null> = useRef(null)
   if(!elRef.current) {
     elRef.current = document.createElement('div')
   }
 
   useEffect(() => {
     const modalRoot = document.querySelector('#modal')
+    if(!modalRoot || !elRef.current) return
     modalRoot.appendChild(elRef.current)
     // if we return something in useEffect
     // it will trigger when component unmount!
     // usually use for memory leak such as remove dom or stop timer
     return () => {
+      if(!elRef.current) return
       modalRoot.removeChild(elRef.current)
     }
   }, [])
